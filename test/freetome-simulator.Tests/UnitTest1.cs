d89fd711b3951bef15bdde2d5f6085cd3eb4807c
wiseby <l.wiseby@gamil.com>
2019-10-09 15:33:06 +0200

    directory structure finished

---
+++ test/freetome-simulator.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace freetome_simulator.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
