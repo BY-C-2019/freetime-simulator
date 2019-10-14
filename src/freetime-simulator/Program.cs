a38bdef4e251450f2a7f9762d5679784739d2ba4
Lucas <l.wiseby@gmail.com>
2019-10-14 09:42:19 +0200

    Visualizing code as comments.

--- src/freetime-simulator/Program.cs
+++ src/freetime-simulator/Program.cs
@@ -6,7 +6,27 @@ namespace freetime_simulator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("===== FreeTime Simulator =====");
+            
+            // Skriv ut instructioner
+
+            // Be användaren om en lista med object.
+
+            // Initiering.
+
+            // Starta experimentet.
+
+            // Be användaren att använde en pryl.
+
+            // Utför aktivitet.
+
+            // Är experimentet slut?
+
+            // Är experimentet slut så sammanfatta statistik.
+
+            // Alternativt spara data i textfil.
+            
+            // Avsluta.
         }
     }
 }
