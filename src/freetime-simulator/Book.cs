da8249363db8e144f17bac97eb794c0cea8cf823
wiseby <l.wiseby@gamil.com>
2019-10-15 11:59:41 +0200

    Book class progressed

--- src/freetime-simulator/Book.cs
+++ src/freetime-simulator/Book.cs
@@ -1,7 +1,21 @@
 namespace freetime_simulator
 {
-    class Book
+    class Book : Media
     {
-        
+        public string Title { get; }
+        public string Author { get; }
+        public int Pages { get; }
+        public Book(int length, string title, string author, int pages) : base(length)
+        {
+            Title = title;
+            Author = author;
+            Pages = pages;
+        }
+
+        public override string ToString()
+        {
+            string str = $"Title: {Title} | Author: {Author} | Number Of Pages: {Pages}";
+            return str;
+        }
     }
 }
