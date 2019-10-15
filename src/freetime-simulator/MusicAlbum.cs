a69623d7ba17bad22845d112f764a8b044e5ad54
wiseby <l.wiseby@gamil.com>
2019-10-15 12:00:24 +0200

    MusicAlbum class progressed

--- src/freetime-simulator/MusicAlbum.cs
+++ src/freetime-simulator/MusicAlbum.cs
@@ -1,7 +1,23 @@
-namespace fretime_simulator
+namespace freetime_simulator
 {
-    class MusicAlbum
+    class MusicAlbum : Media
     {
+        public string Artist { get; }
+        public MusicAlbum(int length, string title, string artist) : base(length, title)
+        {
+               Artist = artist;
+        }
+
+        public override bool MediaPlayable(LivingRoom room)
+        {
+            return true;
+        }
         
+
+        public override string ToString()
+        {
+            string str = $"Title: {Title} | Artist: {Artist} | Length: {Length}";
+            return str;
+        }
     }
 }
