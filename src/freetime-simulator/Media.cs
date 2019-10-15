eed47c494b651ba1c358c6a0d7182542da331385
wiseby <l.wiseby@gamil.com>
2019-10-15 11:59:52 +0200

    Media class progressed

--- src/freetime-simulator/Media.cs
+++ src/freetime-simulator/Media.cs
@@ -2,11 +2,18 @@ namespace freetime_simulator
 {
     class Media
     {
-        public int Length { get; set; }
+        public int Length { get; }
+        public string Title { get; }
 
-        public Media(int length)
+        public Media(int length, string title)
         {
             Length = length;
+            Title = title;
+        }
+
+        public virtual bool MediaPlayable(LivingRoom room)
+        {
+            return true;
         }
     }
 }
