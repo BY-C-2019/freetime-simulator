da96c9c3e80bd26f93ae01ad68baf737e6c17955
wiseby <l.wiseby@gamil.com>
2019-10-15 12:00:05 +0200

    Movie class progressed

--- src/freetime-simulator/Movie.cs
+++ src/freetime-simulator/Movie.cs
@@ -1,7 +1,25 @@
+using System;
+
 namespace freetime_simulator
 {
-    class Name
-    {
-        
+    class Movie : Media
+    {   
+        public DateTime ReleaseDate { get; }     
+        public Movie(int length, string title, DateTime releaseDate) : base(length, title)
+        {
+            ReleaseDate = releaseDate;
+        }
+
+        public override bool MediaPlayable(LivingRoom room)
+        {
+            // TODO if a DVD and TV exists in the LivingRoom
+            return true;
+        }
+
+        public override string ToString()
+        {
+            string str = $"Title: {Title} | Author: {ReleaseDate} | Length: {Length}";
+            return str;
+        }
     }
 }
